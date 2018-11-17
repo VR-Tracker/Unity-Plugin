ccaae6b47aac8d2b6c4f04eacad4f71b6f9c6001
Alex <alex@vrtracker.xyz>
2018-11-17 15:24:58 -0500

    fix consol error + revert to old prefabs

--- Player/Scripts/VRT_PlayerInstance.cs
+++ Player/Scripts/VRT_PlayerInstance.cs
@@ -5,9 +5,7 @@ using UnityEngine.UI;
 using VRTracker.Network;
 using VRTracker.Manager;
 using System.Collections.Generic;
-#if VRTRACKER_INTERNAL
 using VRTracker.Scoreboard;
-#endif
 
 namespace VRTracker.Player
 {
@@ -29,33 +27,34 @@ namespace VRTracker.Player
         [SyncVar(hook = "OnChangeName")] public string playerName;
         public event EventHandler<StringDataEventArgs> nameChangeEvent;
         public Text playerNameText; //assign text field to display name above player
-        Vector3 velocity;			
+        Vector3 velocity;
         Vector3 previousVelocity;
-		public int playerId;		//Player unique id
-		public int playerTeamId;	//Player's team unique id
+        public int playerId;        //Player unique id
+        public int playerTeamId;    //Player's team unique id
 
-    
+        [SerializeField] public VRT_PlayerModelSkin playerSkin;
         [SerializeField] public NetworkIdentity networkIdentity;
+        [SerializeField] public VRT_PlayerStats playerStats;
 
-		public void Start()
+        public void Start()
         {
             if (!isLocalPlayer)
                 playerNameText.text = playerName;
 
-         }
-			
+        }
+
         void OnChangeName(string newName)
         {
+            //Debug.Log("change name hook");
             playerName = newName;
             NameChangeEvent(new StringDataEventArgs(newName));
             playerNameText.text = newName;
-
         }
 
-		/// <summary>
-		/// Raises the name change event.
-		/// </summary>
-		/// <param name="e">E.</param>
+        /// <summary>
+        /// Raises the name change event.
+        /// </summary>
+        /// <param name="e">E.</param>
         void NameChangeEvent(StringDataEventArgs e)
         {
             EventHandler<StringDataEventArgs> handler = nameChangeEvent;
@@ -63,10 +62,16 @@ namespace VRTracker.Player
                 handler(this, e);
         }
 
-		/// <summary>
-		/// Raises the ready state change event.
-		/// </summary>
-		/// <param name="ready">If set to <c>true</c> ready.</param>
+        public void SetName(string name)
+        {
+            playerName = name;
+            if(isServer)
+                NameChangeEvent(new StringDataEventArgs(name));
+        }
+        /// <summary>
+        /// Raises the ready state change event.
+        /// </summary>
+        /// <param name="ready">If set to <c>true</c> ready.</param>
         void OnReadyStateChange(bool ready)
         {
             if (!isServer) //In case server and client
@@ -77,10 +82,10 @@ namespace VRTracker.Player
 
         }
 
-		/// <summary>
-		/// Raises the ready state change event.
-		/// </summary>
-		/// <param name="e">E.</param>
+        /// <summary>
+        /// Raises the ready state change event.
+        /// </summary>
+        /// <param name="e">E.</param>
         void ReadyStateChangeEvent(EventArgs e)
         {
             EventHandler<EventArgs> handler = readyStateEvent;
@@ -88,37 +93,39 @@ namespace VRTracker.Player
                 handler(this, e);
         }
 
-		/// <summary>
-		/// Sets the player to ready state
-		/// And raises the ready state change event
-		/// </summary>
+        /// <summary>
+        /// Sets the player to ready state
+        /// And raises the ready state change event
+        /// </summary>
         public void SetReady()
         {
             readyState = true;
             ReadyStateChangeEvent(EventArgs.Empty);
         }
 
-		/// <summary>
-		/// Sets the player to not ready state
-		/// And raises the ready state change event
-		/// </summary>
+        /// <summary>
+        /// Sets the player to not ready state
+        /// And raises the ready state change event
+        /// </summary>
         public void SetNotReady()
         {
             readyState = false;
             ReadyStateChangeEvent(EventArgs.Empty);
         }
 
-		/// <summary>
-		/// Inform the user to reorient its view
-		/// This function is called from a server command
-		/// </summary>
-		/// <param name="con">Con.</param>
+        /// <summary>
+        /// Inform the user to reorient its view
+        /// This function is called from a server command
+        /// </summary>
+        /// <param name="con">Con.</param>
         [TargetRpc]
         public void TargetReoriente(NetworkConnection con)
         {
             VRT_HeadsetRotation headsetRotation = GetComponentInChildren<VRT_HeadsetRotation>();
+            Debug.Log("targetReorient " + (headsetRotation == null));
             if (headsetRotation != null)
             {
+                
                 headsetRotation.ResetOrientation();
             }
         }
