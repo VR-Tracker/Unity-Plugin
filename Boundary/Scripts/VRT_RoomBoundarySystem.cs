4f53ed50bbe343bafb1991a315cd368338061782
Unknown <mathieu@vrtracker.xyz>
2018-11-19 15:57:37 -0500

    fix boundaries wall
    Fixed incorect distance calculation to player
    Removed multiple call to getcomponent for optimization

--- Boundary/Scripts/VRT_RoomBoundarySystem.cs
+++ Boundary/Scripts/VRT_RoomBoundarySystem.cs
@@ -37,9 +37,9 @@ namespace VRTracker.Boundary
         Coroutine alertRoutine;
         bool alertSoundPlaying = false;
 
-		public static event Action OnNewBoundaries;  // Called when new boundaries are set
-		public static bool exist = false; //Variable to know if the object exist, knowing if the data has been updated
-											//WARNING: This will be removed in a next update
+        public static event Action OnNewBoundaries;  // Called when new boundaries are set
+        public static bool exist = false; //Variable to know if the object exist, knowing if the data has been updated
+                                          //WARNING: This will be removed in a next update
 
 
         // Generate the walls using data of each corner
@@ -49,13 +49,13 @@ namespace VRTracker.Boundary
 
             walls = new List<GameObject>();
             LoadCornersData();
-			//Don't draw the boundaries for spectator
+            //Don't draw the boundaries for spectator
             if (!VRTracker.Manager.VRT_Manager.Instance.spectator)
             {
                 DrawBoundaries();
             }
-			OnNewBoundaries += UpdateBoundaries;
-			exist = true;
+            OnNewBoundaries += UpdateBoundaries;
+            exist = true;
         }
 
         public void UpdateBoundaries()
@@ -64,15 +64,15 @@ namespace VRTracker.Boundary
             DrawBoundaries();
         }
 
-		/// <summary>
-		/// Load the boundary and return the corners data 
-		/// </summary>
-		/// <returns>The corners data.</returns>
+        /// <summary>
+        /// Load the boundary and return the corners data 
+        /// </summary>
+        /// <returns>The corners data.</returns>
         JSONNode LoadCornersData()
         {
             JSONNode cornerList = null;
             string filePath = Path.Combine(Application.persistentDataPath, JsonFilePath);
-			Debug.Log(filePath);
+            Debug.Log(filePath);
             if (File.Exists(filePath))
             {
                 // Read the json from the file into a string
@@ -92,7 +92,7 @@ namespace VRTracker.Boundary
                 {
                     cornersLoaded();
                 }
-
+                //Debug.Log("corners loaded");
             }
             else
             {
@@ -101,16 +101,18 @@ namespace VRTracker.Boundary
             return cornerList;
         }
 
-		/// <summary>
-		/// Draws the boundaries.
-		/// </summary>
+        /// <summary>
+        /// Draws the boundaries.
+        /// </summary>
         void DrawBoundaries()
         {
+            Debug.Log("draw boundary");
             walls.Clear();
             for (int i = 0; i < corners.Count; i++)
             {
-                float distanceToNextPoint = Mathf.Sqrt((corners[i].x - corners[(i + 1) % corners.Count].x) * (corners[i].x - corners[(i + 1) % corners.Count].x) 
-											+ (corners[i].z - corners[(i + 1) % corners.Count].z) * (corners[i].z - corners[(i + 1) % corners.Count].z));
+                //Debug.Log("drawing corner " + (i + 1) + " out of " + corners.Count);
+                float distanceToNextPoint = Mathf.Sqrt((corners[i].x - corners[(i + 1) % corners.Count].x) * (corners[i].x - corners[(i + 1) % corners.Count].x)
+                                            + (corners[i].z - corners[(i + 1) % corners.Count].z) * (corners[i].z - corners[(i + 1) % corners.Count].z));
                 float angle = Vector3.Angle(Vector3.left, corners[(i + 1) % corners.Count] - corners[i]);
 
                 GameObject bound = (GameObject)Instantiate(boundaryWall);
@@ -120,13 +122,13 @@ namespace VRTracker.Boundary
                 scale.x = distanceToNextPoint / 10;
                 bound.transform.localScale = scale;
 
-				//Set the boundarie gameobject position
+                //Set the boundarie gameobject position
                 Vector3 position = bound.transform.position;
                 position.x = corners[i].x + (corners[(i + 1) % corners.Count].x - corners[i].x) / 2;
                 position.z = corners[i].z + (corners[(i + 1) % corners.Count].z - corners[i].z) / 2;
                 bound.transform.position = position;
 
-				//Set the boundarie gameobject rotation
+                //Set the boundarie gameobject rotation
                 Vector3 rotation = bound.transform.localRotation.eulerAngles;
                 rotation.y = angle;
                 bound.transform.localRotation = Quaternion.Euler(rotation);
@@ -137,7 +139,7 @@ namespace VRTracker.Boundary
 
                 walls.Add(bound);
             }
-			/*
+            /*
 			 * Enable sound when player go outside the boundaries
                 if(walls.Count > 1)
                 {
@@ -147,26 +149,44 @@ namespace VRTracker.Boundary
                 */
             ChangeGlobalWallsAlpha(1.0f);
         }
-			
-		/// <summary>
-		/// Changes the global walls alpha.
-		/// </summary>
-		/// <param name="value">Value.</param>
-        void ChangeGlobalWallsAlpha(float value)
+
+
+        bool assigned;
+        List<TextureAlpha> textureAlphaList = new List<TextureAlpha>();
+
+        void AssignTextureAlphaScript()
         {
-            generalAlpha = value;
             foreach (GameObject wall in walls)
             {
-                TextureAlpha textureAlpha = wall.GetComponent<TextureAlpha>();
-                if (textureAlpha)
-                    textureAlpha.SetAlpha(value);
+                textureAlphaList.Add(wall.GetComponent<TextureAlpha>());
             }
         }
-			
-		/// <summary>
-		/// Gets the shortest distance to walls and return the shortest distance;
-		/// </summary>
-		/// <returns>The shortest distance to walls.</returns>
+
+        /// <summary>
+        /// Changes the global walls alpha.
+        /// </summary>
+        /// <param name="value">Value.</param>
+        void ChangeGlobalWallsAlpha(float value)
+        {
+            //Debug.Log("change alpha value in roomboundary script to " + value);
+            generalAlpha = value;
+
+            if(!assigned)
+            {
+                assigned = true;
+                AssignTextureAlphaScript();
+            }
+
+            foreach (TextureAlpha wall in textureAlphaList)
+            {                
+                    wall.SetAlpha(value);
+            }
+        }
+
+        /// <summary>
+        /// Gets the shortest distance to walls and return the shortest distance;
+        /// </summary>
+        /// <returns>The shortest distance to walls.</returns>
         public float GetShortestDistanceToWalls()
         {
             float distance = 1000.0f;
@@ -178,7 +198,7 @@ namespace VRTracker.Boundary
                 foreach (GameObject wall in walls)
                 {
                     // Get the distance from point to plane by using projection
-                    Vector3 V1 = tag.transform.position - wall.transform.position;
+                    Vector3 V1 = tag.trackedEndpoints[0].transform.position - wall.transform.position;
                     Vector3 V2 = Vector3.Project(V1, wall.transform.up);
                     if (distance > V2.magnitude)
                         distance = V2.magnitude;
@@ -191,23 +211,27 @@ namespace VRTracker.Boundary
         void Update()
         {
             float distance = GetShortestDistanceToWalls();
+
+            
             if (distance < distanceStartFade)
             {
                 float closeToWallValue = (float)(distanceStartFade - distanceStartFade * (distance / distanceStartFade));
 
                 ChangeGlobalWallsAlpha(closeToWallValue);
+                //Debug.Log("Update function distance from wall : " + distance + " alpha set to : " + closeToWallValue);
 
             }
             else if (generalAlpha != 0.0f)
             {
+                //Debug.Log("Update function distance from wall : " + distance + " alpha set to zero ");
                 ChangeGlobalWallsAlpha(0.0f);
             }
         }
 
-		/// <summary>
-		/// Launch the alert sound
-		/// For when the player is getting to close to the boundaries
-		/// </summary>
+        /// <summary>
+        /// Launch the alert sound
+        /// For when the player is getting to close to the boundaries
+        /// </summary>
         public void PlayAlert()
         {
             if (!alertSoundPlaying)
@@ -217,10 +241,10 @@ namespace VRTracker.Boundary
             }
         }
 
-		/// <summary>
-		/// Stop the alert sound 
-		/// For when player is getting far enough of the boundaries
-		/// </summary>
+        /// <summary>
+        /// Stop the alert sound 
+        /// For when player is getting far enough of the boundaries
+        /// </summary>
         public void StopAlert()
         {
             if (alertSoundPlaying)
@@ -244,65 +268,65 @@ namespace VRTracker.Boundary
             }
         }
 
-		/// <summary>
-		/// Saves the boundaries into a Json file
-		/// </summary>
-		/// <param name="content">boundaries file data in string</param>
+        /// <summary>
+        /// Saves the boundaries into a Json file
+        /// </summary>
+        /// <param name="content">boundaries file data in string</param>
         public static void SaveBoundaries(string content)
         {
             string filePath = Path.Combine(Application.persistentDataPath, JsonFilePath);
             File.WriteAllText(filePath, content);
-			if (OnNewBoundaries != null)
-				OnNewBoundaries ();
+            if (OnNewBoundaries != null)
+                OnNewBoundaries();
         }
 
-		/// <summary>
-		/// Creates the boundaries from limit send from the dashboard
-		/// Store the data in the corners_data file
-		/// </summary>
-		/// <param name="xmin">Xmin value for boundary</param>
-		/// <param name="xmax">Xmax value for boundary</param>
-		/// <param name="ymin">Ymin value for boundary</param>
-		/// <param name="ymax">Ymax value for boundary</param>
-		public static void CreateBoundariesFromLimit(float xmin, float xmax, float ymin, float ymax)
-		{
-			//Create the date to save in the coners_data file
-			JSONNode cornerList = new JSONArray();
-			JSONNode cornerValue = new JSONObject();
+        /// <summary>
+        /// Creates the boundaries from limit send from the dashboard
+        /// Store the data in the corners_data file
+        /// </summary>
+        /// <param name="xmin">Xmin value for boundary</param>
+        /// <param name="xmax">Xmax value for boundary</param>
+        /// <param name="ymin">Ymin value for boundary</param>
+        /// <param name="ymax">Ymax value for boundary</param>
+        public static void CreateBoundariesFromLimit(float xmin, float xmax, float ymin, float ymax)
+        {
+            //Create the date to save in the coners_data file
+            JSONNode cornerList = new JSONArray();
+            JSONNode cornerValue = new JSONObject();
 
-			cornerValue["x"] = xmin;
-			cornerValue["z"] = ymin;
-			cornerList.Add(cornerValue);
-			cornerValue = new JSONObject();
+            cornerValue["x"] = xmin;
+            cornerValue["z"] = ymin;
+            cornerList.Add(cornerValue);
+            cornerValue = new JSONObject();
 
-			cornerValue["z"] = ymax;
-			cornerList.Add(cornerValue);
-			cornerValue = new JSONObject();
+            cornerValue["z"] = ymax;
+            cornerList.Add(cornerValue);
+            cornerValue = new JSONObject();
 
-			cornerValue["x"] = xmax;
-			cornerList.Add(cornerValue);
-			cornerValue = new JSONObject();
+            cornerValue["x"] = xmax;
+            cornerList.Add(cornerValue);
+            cornerValue = new JSONObject();
 
-			cornerValue["z"] = ymin;
-			cornerList.Add(cornerValue);
+            cornerValue["z"] = ymin;
+            cornerList.Add(cornerValue);
 
-			string filePath = Path.Combine(Application.persistentDataPath, JsonFilePath);
-			string content = cornerList.ToString();
-			if (!File.Exists (filePath)) 
-			{
-				File.WriteAllText (filePath, content);
-				if (OnNewBoundaries != null)
-					OnNewBoundaries ();
-			} 
-			else 
-			{
-				if (VRT_RoomBoundarySystem.exist) 
-				{
-					if (OnNewBoundaries != null)
-						OnNewBoundaries ();
-				}
-				//In the other case it is just the file that has been sent by the Gateway when started
-			}
-		}
+            string filePath = Path.Combine(Application.persistentDataPath, JsonFilePath);
+            string content = cornerList.ToString();
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, content);
+                if (OnNewBoundaries != null)
+                    OnNewBoundaries();
+            }
+            else
+            {
+                if (VRT_RoomBoundarySystem.exist)
+                {
+                    if (OnNewBoundaries != null)
+                        OnNewBoundaries();
+                }
+                //In the other case it is just the file that has been sent by the Gateway when started
+            }
+        }
     }
 }
