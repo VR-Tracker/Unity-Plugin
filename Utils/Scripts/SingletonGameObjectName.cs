3f9ee315798a22f58a48676a18755e32dedc4499
Unknown <mat.pay@Hotmail.ca>
2019-01-09 13:57:26 -0500

    Add scripty to avoid duplicatesd in gameobject when going thorugh pairing and coming back to first scene

---
+++ Utils/Scripts/SingletonGameObjectName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* This script will look for other gameobject with the same name on awake
+ * and auto destroy if another is found.
+ * The goal is to avoid duplicate gameobjects when moving to the pairing scene and back
+ * to the original scene
+ */
+namespace VRTracker.Utils
+{
+    public class SingletonGameObjectName : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Awake()
+        {
+            int count = 0;
+            foreach (var gameObj in FindObjectsOfType(typeof(GameObject)) as GameObject[])
+            {
+                if (gameObj.name == this.gameObject.name)
+                    count++;
+            }
+            if(count > 1)
+                Destroy(gameObject);
+        }
+        
+    }
+}
